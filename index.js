32319e75e95e0868c93c208937a090ca775c95fb
Konstantin Vassiliev <konstantin.vassiliev@remotex.net>
2017-08-24 15:28:13 +0200

    Working version 1

--- index.js
+++ index.js
@@ -1,29 +1,37 @@
 'use strict';
 
 const path = require('path');
+const fs = require('fs');
 const jade = require('jade');
-const debug = require('debug')('brunch:views-brunch');
 
-debug('here');
-console.log('here');
+const defaults = {
+  path: 'views',
+  ext: 'jade'
+};
 
 class Views {
   constructor(config) {
-    this.config = config.plugins.views || {};
-
-    debug('constructor: ', this.config);
-    console.log('constructor: ', this.config);
+    this.config = Object.assign({}, defaults, config.plugins.views);
   }
 
   compile(file) {
-    debug('compile: ', file);
-    console.log('compile: ', file);
+    let name, data, template;
+
+    try {
+      name = path.parse(file.path).name;
+      data = JSON.parse(file.data);
+      template = fs.readFileSync(path.join(`${this.config.path}`, `${name}.${this.config.ext}`), 'utf-8');
+    }
+    catch (error) {
+      return Promise.reject(`${error}`);
+    }
+
+    return Promise.resolve('module.exports = ' + JSON.stringify(jade.render(template, data.definitions)) + ';');
   }
 }
 
 Views.prototype.brunchPlugin = true;
 Views.prototype.type = 'template';
-Views.prototype.pattern = /\.jage$/;
-Views.prototype.targetExtension = 'html';
+Views.prototype.pattern = /\.json$/;
 
 module.exports = Views;
