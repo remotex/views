1e623532b7d18a619fa8c5366c7333c4c8a8d306
Konstantin Vassiliev <konstantin.vassiliev@remotex.net>
2017-08-28 14:18:50 +0200

    First version of Views plug-in. (#1)
    * Initial setup
    
    * Removes trainling comma
    
    * Trying to make logging work
    
    * removes main cause it is index.js
    
    * updates package.json
    
    * debug
    
    * Working version 1
    
    * Defaults template to definitions file name
    
    * uses jade renderFile
    
    * Adds readme. Updates pattern for watched files.

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# views
+# views-brunch
+
+Dynamically renders templates into html strings and makes them available in `require`.
+Takes in a template and a definition file.
+
+## Options
+`templates` (string)
+  Path to folder with templates. Defaults to `'views'`
+
+`definitions` (pattern)
+  Pattern to folder with definitions files. Defaults to `/^app\/templates[\\/].*\.json$/`
+
+`ext`
+  Extension of template files. Defaults to `'jade'`
+
+## Definitions
+Definitions are `json` files with data to be interpolated into templates.
+
+Can have an optional property `template` (string) which holds template file name to be used with given definitions object. Defaults to definitions file name.
---
+++ index.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const path = require('path');
+const jade = require('jade');
+
+const defaults = {
+  templates: 'views',
+  definitions: /^app\/templates[\\/].*\.json$/,
+  ext: 'jade'
+};
+
+class Views {
+  constructor(config) {
+    this.config = Object.assign({}, defaults, config.plugins.views);
+    this.pattern = this.config.definitions;
+  }
+
+  compile(file) {
+    let definitions, name, template;
+
+    try {
+      definitions = JSON.parse(file.data);
+      name = definitions.template || path.parse(file.path).name;
+      template = path.join(`${this.config.templates}`, `${name}.${this.config.ext}`);
+    }
+    catch (error) {
+      return Promise.reject(`${error}`);
+    }
+
+    return Promise.resolve('module.exports = ' + JSON.stringify(jade.renderFile(template, definitions)) + ';');
+  }
+}
+
+Views.prototype.brunchPlugin = true;
+Views.prototype.type = 'template';
+
+module.exports = Views;
